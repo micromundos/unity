2c450b1305e87a4a670c23ccb2191fb4bb18a4a2
micromundos <micromundos@xxx>
2015-08-28 16:02:18 -0300

    slider fps

--- Assets/SyphonParser.cs
+++ Assets/SyphonParser.cs
@@ -1,10 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class SyphonParser : MonoBehaviour {
 
 	//	in
 	public	GameObject	syphon_holder;	//el objeto que tenga el script de syphon asociado
+	public	Slider		slider_fps;
+	public  Text		text_slider;
 	//	out
 	private Texture2D	surface_map;
 
@@ -16,7 +19,7 @@ public class SyphonParser : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-		if (Time.frameCount % 3 == 0) {
+		if (slider_fps.value == 0 || (slider_fps.value > 0 && Time.frameCount % 2 == slider_fps.value)) {
 
 			RenderTexture texture = syphon_holder.GetComponent<SyphonClientTexture> ().clientObject.AttachedTexture;
 			RenderTexture currentActiveRT = RenderTexture.active;
@@ -24,10 +27,10 @@ public class SyphonParser : MonoBehaviour {
 			surface_map = new Texture2D (texture.width, texture.height);
 			surface_map.ReadPixels (new Rect (0, 0, texture.width, texture.height), 0, 0);
 			RenderTexture.active = currentActiveRT;
-	
-
+		
 		}
 
+		text_slider.text = slider_fps.value.ToString ("0");
 	}
 
 	public Texture2D	SurfaceMap()
